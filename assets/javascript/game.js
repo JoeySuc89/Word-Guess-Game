c427be9b97a0b57a592af3357684cca73a1bbd85
JoeySuc89 <jsucameli1@gmail.com>
2019-03-08 22:47:22 -0500

    added a letters array and made it so the wrong guesses appear inside another array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,8 @@
-var wordBank = ["Superman", "Batman", "Aquaman", "Iron Man", "Antman", "Cyborg", "Flash", "Cyborg", "Captain America", "Hulk", "Black Panther", "Shazam", "Captain Marvel", "Doctor Strange", "Black Widow", "Wonder Woman"];
+var wordBank = ["superman", "batman", "aquaman", "ironman", "antman", "cyborg", "flash", "captain america", "hulk", "black panther", "shazam", "captain marvel", "doctor strange", "black widow", "wonder woman"];
 var rightGuess = [];
 var wrongGuess = [];
-var underScore = [];
+var guess; //user guess
+var letterKeys = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",]
 console.log(wordBank)
 
 function getRanWord() {
@@ -12,3 +13,36 @@ function getRanWord() {
 var randomWord = getRanWord()
 
 console.log(randomWord)
+
+function start() {
+  for (i = 0; i < randomWord.length; i++) {
+    rightGuess[i] = "_";
+  }
+  document.getElementById("answer").innerHTML = rightGuess.join(" ");
+  console.log(wordBank)
+}
+
+function checkLetter() {
+  document.onkeyup = function(event) {
+    guess = event.key.toLowerCase();
+    var found = false;
+    if (letterKeys.indexOf(guess) !== -1){
+      for(i = 0; i < randomWord.length; i++){
+        if (guess === randomWord[i]){
+          rightGuess[i] = guess;
+          document.getElementById("answer").innerHTML = rightGuess.join(" ");
+          found = true;
+        }
+      }
+      if (found) return;
+      if (wrongGuess.indexOf(guess) < 0) {
+        wrongGuess.push(guess);
+        document.getElementById("incorrect").innerHTML = wrongGuess.join(" ");
+
+     }
+    }
+  }
+}
+
+start();
+checkLetter();
